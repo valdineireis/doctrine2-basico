d079176029487feb1cceab02d3539ebaa7cfa566
Valdinei Reis <valdineimtz@gmail.com>
2016-07-14 22:54:28 -0300

    Editando uma categoria

--- src/routes.php
+++ src/routes.php
@@ -53,6 +53,32 @@ $map->post('categories.store', '/categories/store',
 	}
 );
 
+$map->get('categories.edit', '/categories/{id}/edit', 
+	function(ServerRequestInterface $request, $response) use ($view, $entityManager) {
+		$id = $request->getAttribute('id');
+		$repository = $entityManager->getRepository(Category::class);
+		$category = $repository->find($id);
+		return $view->render($response, 'categories/edit.phtml', [
+			'category' => $category
+		]);
+});
+
+$map->post('categories.update', '/categories/{id}/update', 
+	function(ServerRequestInterface $request, $response) use ($view, $entityManager, $genarator) {
+		$id = $request->getAttribute('id');
+		$repository = $entityManager->getRepository(Category::class);
+		$category = $repository->find($id);
+
+		$data = $request->getParsedBody();
+
+		$category->setNome($data['nome']);
+		$entityManager->flush();
+
+		$uri = $genarator->generate('categories.list');
+		return new Response\RedirectResponse($uri);
+	}
+);
+
 $matcher = $routerContainer->getMatcher();
 
 $route = $matcher->match($request);

6783856f8f8439bc57cc024418d9a1ac14f6e9f3
Valdinei Reis <valdineimtz@gmail.com>
2016-07-15 00:09:32 -0300

    Formulário para seleção das categorias

--- src/posts.php
+++ src/posts.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Entity\Post;
+use App\Entity\Category;
 use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\Response;
 
@@ -69,6 +70,40 @@ $map->get('posts.remove', '/posts/{id}/remove',
 		$entityManager->flush();
 
 		$uri = $genarator->generate('posts.list');
+		return new Response\RedirectResponse($uri);
+	}
+);
+
+$map->get('posts.categories', '/posts/{id}/categories', 
+	function(ServerRequestInterface $request, $response) use ($view, $entityManager, $genarator) {
+		$id = $request->getAttribute('id');
+
+		$repository = $entityManager->getRepository(Post::class);
+		$categoryRepository = $entityManager->getRepository(Category::class);
+
+		$categories = $categoryRepository->findAll();
+		$post = $repository->find($id);
+
+		return $view->render($response, 'posts/categories.phtml', [
+			'post' => $post,
+			'categories' => $categories
+		]);
+	}
+);
+
+$map->post('posts.set-categories', '/posts/{id}/set-categories', 
+	function(ServerRequestInterface $request, $response) use ($view, $entityManager, $genarator) {
+		$id = $request->getAttribute('id');
+		$repository = $entityManager->getRepository(Post::class);
+		$post = $repository->find($id);
+
+		$data = $request->getParsedBody();
+
+		/*$post->setTitulo($data['titulo'])
+			->setConteudo($data['conteudo']);
+		$entityManager->flush();
+
+		$uri = $genarator->generate('posts.list');*/
 		return new Response\RedirectResponse($uri);
 	}
 );
